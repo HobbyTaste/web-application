be9f8f69e14908aeec91560a25637578cfa7a9af
YankinaDarya <yankina109@gmail.com>
2019-12-07 21:45:30 +0300

    babel error

--- static/components/Footer/Contacts/Button.jsx
+++ static/components/Footer/Contacts/Button.jsx
@@ -1,13 +1,49 @@
-import React, {Component} from 'react';
-import s from './Button.module.css';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import style from './Button.module.css';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Slide from '@material-ui/core/Slide';
+import {ProviderRegistration} from "../../ProviderRegistration/ProviderRegistration";
+import {GreenButton, GreenLargeButton} from "../../Common/MaterialsButtons";
 
-const Button = (props) => {
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        position: 'relative',
+    }
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export const FullScreenDialogButton = (props) => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <div>
-            <Link to='/provider_registration'>
-                <button className={s.button}>ЗАРЕГЕСТРИРОВАТЬСЯ КАК ПАРТНЕР</button></Link>
-        </div>);
+            <div onClick={handleClickOpen} >
+                <GreenLargeButton text={"РЕГИСТРАЦИЯ И ВХОД ПАРТНЕРА"} />
+            </div>
+            <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+                <AppBar className={classes.appBar}>
+                    <div className={style.header}>
+                        <div className={style.headerTitle}>Вход в личный кабинет партнера</div>
+                        <div className={style.signIn}>
+                            <div onClick={handleClose} >
+                                <GreenButton text={"ВОЙТИ"} />
+                            </div>
+                        </div>
+                    </div>
+                </AppBar>
+                <ProviderRegistration />
+            </Dialog>
+        </div>
+    );
 };
-
-export default Button;
